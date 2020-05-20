4159bd8f66c75a8fd95255b5d3f606d1be80411f
Roberto <rovalos@gmail.com>
2020-05-20 10:56:09 -0500

    ask for location permissions

--- app/src/main/java/com/tangosource/mapboxapp/MainActivity.kt
+++ app/src/main/java/com/tangosource/mapboxapp/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.tangosource.mapboxapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PermissionsListener {
+
+    private var permissionsManager: PermissionsManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        permissionsManager?.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(applicationContext, "This app needs location permissions", Toast.LENGTH_LONG)
+            .show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (!granted) {
+            Toast.makeText(
+                applicationContext,
+                "You didn\'t grant location permissions.",
+                Toast.LENGTH_LONG
+            ).show()
+            return
+        }
     }
 }
